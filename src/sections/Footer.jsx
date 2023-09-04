31592ecc803de3882670dfd2952edaf99b3b5563
keufack <ngimdockkeufack@gmail.com>
2023-09-04 09:27:30 +0100

    finished-footer-section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,6 @@
+import { copyrightSign } from "../assets/icons";
 import { footerLogo } from "../assets/images";
-import { socialMedia } from "../constants";
+import { footerLinks, socialMedia } from "../constants";
 const Footer = () => {
   return (
     <footer className="max-container">
@@ -21,6 +22,37 @@ const Footer = () => {
             ))}
           </div>
         </div>
+        <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
+          {footerLinks.map((section) => (
+            <div key={section}>
+              <h4 className="text-white font-montserrat text-2xl leading-normal font-medium mb-6">
+                {section.title}
+              </h4>
+              <ul>
+                {section.links.map((link) => (
+                  <li className="mt-3 text-white-400 font-montserrat text-base cursor-pointer duration-400 leading-normal hover:text-slate-gray">
+                    <a>{link.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+        <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+          <img
+            src={copyrightSign}
+            alt="copy right sign"
+            width={20}
+            height={20}
+            className="rounded-full m-0"
+          />
+          <p>copyright all right deserve</p>
+        </div>
+        <p className="underline text-xl text-slate-400 font-montserrat cursor-pointer ">
+          DEV BY KEUFACK::::
+        </p>
       </div>
     </footer>
   );
